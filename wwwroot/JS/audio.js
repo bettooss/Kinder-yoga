e6394cc232b761f872d79b9062131d321a59fc91
Roberto Acuña <robertoaalarcon@live.com>
2024-01-01 15:50:51 -0600

    mi primer GIT

---
+++ wwwroot/JS/audio.js
@@ -0,0 +1,132 @@
+﻿
+
+let playlist = [{
+    'title': 'Silenciadasaso',
+    'audio': "./audio/Silencio.mp3",
+}, {
+    'title': 'Mulholland - King Canyon',
+    'audio': "./audio/Silencio.mp3",
+}, {
+    'title': 'Silencio',
+    'audio': "./audio/Silencio.mp3",
+}];
+i = 0;
+n = playlist.length;
+let player = document.getElementById('player');
+let dur = document.getElementById('dur');
+playlist.forEach(function (i) {
+    console.log(i.audio)
+    player.src = i.audio;
+    $('.title').html(i.title);
+},);
+
+function calculateTotalValue(length) {
+    let minutes = Math.floor(length / 60),
+        seconds_int = length - minutes * 60,
+        seconds_str = seconds_int.toString(),
+        seconds = seconds_str.substr(0, 2),
+        time = minutes + ':' + seconds
+    return time;
+}
+
+/****************** Asigna tiempo de inicio **************************/
+
+function calculateCurrentValue(currentTime) {
+    let current_hour = parseInt(currentTime / 3600) % 24,
+        current_minute = parseInt(currentTime / 60) % 60,
+        current_seconds_long = currentTime % 60,
+        current_seconds = current_seconds_long.toFixed(),
+        current_time = (current_minute < 10 ? "0" + current_minute : current_minute) + ":" + (current_seconds < 10 ? "0" + current_seconds : current_seconds);
+    return current_time;
+}
+
+/*********************Asigna time de inicio y final*****************************/
+
+function initProgressBar() {
+    //console.log("entra a ini progress");
+
+    let length = player.duration;
+    let current_time = player.currentTime;
+    let totalLength = calculateTotalValue(length)
+    jQuery(".end-time").html(totalLength);
+    let currentTime = calculateCurrentValue(current_time);
+    jQuery(".start-time").html(currentTime);
+    dur.value = player.currentTime;
+    console.log("antes de play");
+    if (player.currentTime == player.duration) {
+        $("#play-btn").fadeIn("slow", function () {
+            console.log("on play");
+            $(this).removeClass("fa-pause");
+            $(this).addClass("fa-play");
+            dur.value = 0;
+        });
+    }
+};
+
+function mSet() {
+    player.currentTime = dur.value;
+}
+
+function mDur() {
+    let length = player.duration;
+    dur.max = length;
+}
+
+function initPlayers(num) {
+    // console.log("Inicio");
+    for (let i = 0; i < num; i++) {
+        (function () {
+            let playerContainer = document.getElementById('player-container'),
+                player = document.getElementById('player'),
+                isPlaying = false,
+                playBtn = document.getElementById('play-btn');
+            if (playBtn != null) {
+    console.log("botol play null");
+                playBtn.addEventListener('click', function () {
+                    console.log("Entra a evento de escucha play");
+                    togglePlay()
+                });
+            }
+
+            function togglePlay() {
+                if (player.paused === false) {
+                    player.pause();
+                    isPlaying = false;
+                    $("#play-btn").fadeIn("slow", function () {
+                        $(this).removeClass("fa-pause");
+                        $(this).addClass("fa-play");
+                    });
+                }
+                else {
+                    player.play();
+                    $("#play-btn").fadeIn("slow", function () {
+                        $(this).removeClass("fa-play");
+                        $(this).addClass("fa-pause");
+                    });
+                    isPlaying = true;
+                }
+            }
+        }());
+    }
+}
+
+
+$(".audio-player")
+    .toArray()
+    .forEach(function (player) {
+        let audio = $(player).find("audio")[0];
+        let volumeControl = $(player).find(".volumeControl .wrapper");
+        volumeControl.find(".outer").on("click", function (e) {
+            let volumePosition = e.pageX - $(this).offset().left;
+            let audioVolume = volumePosition / $(this).width();
+            if (audioVolume >= 0 && audioVolume <= 1) {
+                audio.volume = audioVolume;
+                $(this)
+                    .find(".inner")
+                    .css("width", audioVolume * 100 + "%");
+            }
+        });
+    });
+
+initPlayers(jQuery('#player-container').length);
+
---
+++ wwwroot/JS/play.js
@@ -0,0 +1,168 @@
+﻿
+
+function Alerta1() {
+    alert("Texto a mostrar");
+}
+
+function Alerta2(mensaje) {
+    alert(mensaje);
+}
+
+//var text = document.getElementById("mantra");
+
+//text.addEventListener("click", changetext);
+//function changetext() {
+//    console.log("entra solicitud")
+//    text.innerHTML = "Ocultar mantra"
+//    text.style.color = "red";
+
+//}
+
+
+
+//switch (song) {
+//    case 0:
+//        var i = "./audio/Silencio.mp3";
+//        break;
+//    case 1:
+//        var i = "./audio/formula_premas_om_shanti.mp3";
+//        break;
+//    case 2:
+//        break;
+//    case 3:
+//        break;
+//    default:
+
+//}
+
+//player.onclick = function () {
+
+//    player.
+//    var song = new Array(
+
+
+//    )
+
+
+//    for (int i = 0; ;  )
+//    {
+
+//    }
+
+
+//}
+
+//var song = new Array(
+//    uno,dos,tres,cuatro,cinco,seis.siete,ocho,nueve
+//);
+
+//uno = document.getElementsByClassName('uno');
+//dos = document.getElementsByClassName('dos');
+
+//___________________________________________________________
+let player = document.getElementById('player');
+let dur = document.getElementById('dur');
+let duracion = document.getElementById('duracion');
+
+
+player.preload();
+
+
+
+function iniPlay() {
+    //let length = player.duration;
+    //dur.max = length;
+    
+
+    player.play();
+    //initProgressBar();
+}
+function iniStop() {
+    player.pause();
+}
+
+
+function Vol50(xVol) {
+    //xVol -= .1;
+
+    //console.log("vol:" + xVol);
+    //if (xVol < 0) {
+    //    xVol = 1;
+    //}
+
+    player.volume = xVol;
+
+}
+
+function initProgressBar() {
+    //console.log("entra a ini progress");
+
+    let length = player.duration;
+    let current_time = player.currentTime;
+    let totalLength = calculateTotalValue(length)
+
+    let st = document.getElementById('start-time');
+    let et = document.getElementById('end-time');
+
+
+    //jQuery(".end-time").html(totalLength);
+    let currentTime = calculateCurrentValue(current_time);
+    //jQuery(".start-time").html(currentTime);
+
+    st.innerHTML = currentTime;
+    et.innerHTML = totalLength;
+
+    //dur.value = player.currentTime;
+    //duracion.value = player.currentTime;
+
+    //console.log(dur.value + " de "+ dur.max);
+    console.log(player.currentTime + " de "+ dur.max);
+
+
+    $("#dur").val(player.currentTime);
+
+    //dur.value = player.currentTime;
+};
+
+
+function calculateTotalValue(length) {
+    let minutes = Math.floor(length / 60),
+        seconds_int = length - minutes * 60,
+        seconds_str = seconds_int.toString(),
+        seconds = seconds_str.substr(0, 2),
+        time = minutes + ':' + seconds
+    return time;
+}
+
+
+function calculateCurrentValue(currentTime) {
+    let current_hour = parseInt(currentTime / 3600) % 24,
+        current_minute = parseInt(currentTime / 60) % 60,
+        current_seconds_long = currentTime % 60,
+        current_seconds = current_seconds_long.toFixed(),
+        current_time = (current_minute < 10 ? "0" + current_minute : current_minute) + ":" + (current_seconds < 10 ? "0" + current_seconds : current_seconds);
+    return current_time;
+}
+
+
+function mSet() {
+    var powerRanger = document.getElementById("dur");
+
+    player.currentTime = powerRanger.value;
+
+    console.log("dur:" + powerRanger.value);
+}
+
+
+
+function mDur() {
+    let length = player.duration;
+    dur.max = length;
+
+    var powerRanger = document.getElementById("dur");
+    powerRanger.max = length;
+
+    console.log("Max:"+ length);
+
+    //duracion.max = length;
+}
